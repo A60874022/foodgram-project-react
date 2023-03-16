fdd417e9e3dd0a290dc9db76f3bdb24b1d188bb0
Терницкий Антон <A60874022@yandex.ru>
2023-03-16 22:02:30 +0500

    17.5

--- backend/products/user/admin.py
+++ backend/products/user/admin.py
@@ -5,9 +5,7 @@ from user.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    """
-    Класс для работы таблицы пользователь.
-    """
+    """Класс для работы таблицы пользователь."""
     list_display = ('id', 'username', 'email', 'first_name', 'last_name')
     search_fields = ('username',)
     list_filter = ('username',)
