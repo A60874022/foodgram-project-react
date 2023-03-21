2b5d7bc9e640b161e628ec6fa7f8bd0515d83b25
Терницкий Антон <A60874022@yandex.ru>
2023-03-21 22:12:21 +0500

    17.3

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -7,5 +7,5 @@ class IsAuthenticatedOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-            return (request.user.is_superuser
+        return (request.user.is_superuser
                 or obj.author == request.user)
