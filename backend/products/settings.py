72a0ed4364c21fbdff29cbce98b7d116d095996a
Терницкий Антон <A60874022@yandex.ru>
2023-04-05 21:31:23 +0500

    5

--- backend/products/settings.py
+++ backend/products/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
+
 DEBUG = os.getenv('DEBUG', default=False)
 
 
